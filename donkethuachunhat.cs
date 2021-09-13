029f59798c35451fb644562a621bc3fdd939cb7e
016886611529a <90632819+016886611529a@users.noreply.github.com>
2021-09-13 22:31:14 +0700

    Add files via upload

---
+++ donkethuachunhat.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using static donkethua.Hinhchunhat;
+
+namespace donkethua
+{
+    class Hinhchunhat
+    {
+        public double d, r;
+
+        public Hinhchunhat(double d, double r)
+        {
+            this.d = d;
+            this.r = r;
+        }
+
+        
+        public double dientich()
+        {
+            return d * r;
+        }
+         public class Hinhhop : Hinhchunhat
+        {
+            private double c;
+            public Hinhhop (double d,double r,double c) : base(d,r)
+            {
+                this.c = c;
+
+            }
+            public double dientichtp()
+            {
+                return dientich()* c;
+            }
+
+
+        }
+            
+    }
+    class donkethuachunhat
+    {
+        static void Main(string[] args)
+        {
+            double d, r, c;
+            
+            d = double.Parse(Console.ReadLine());
+            r = double.Parse(Console.ReadLine());
+            c = double.Parse(Console.ReadLine());
+            Hinhhop x = new Hinhhop(d,r,c);
+            Console.WriteLine("dientichhinhtudienla:{0}", x.dientichtp());
+              Console.ReadLine();
+        }
+    }
+}
