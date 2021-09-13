303ad2cd286785b13d5b3df92dc1d9afbe032d0f
016886611529a <90632819+016886611529a@users.noreply.github.com>
2021-09-13 22:33:22 +0700

    Add files via upload

---
+++ kethuahinhtron.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Hoctap
+{
+    class HinhTron
+    {
+        protected double r;
+
+        public HinhTron(double r)
+        {
+            this.r = r;
+        }
+        public double dientich()
+        {
+            return 3.14 * r * r;
+        }
+        public double chuvi()
+        {
+            return 2 * 3.14 * r;
+        }
+    }
+    class HinhTru : HinhTron
+    {
+        private double h;
+        public HinhTru(double r, double h) : base(r)
+        {
+            this.h = h;
+        }
+        public double dientichxungquanh()
+        {
+            return 2 * 3.14 * r * h;// s xung quanh = 2 *pi *r *h; 
+        }
+        public double dientichtoanphan()
+        {
+            return 2 * 3.14 * r * (r + h); //s toàn phần = s xung quanh + 2 mặt đáy 
+        }
+    }
+    class kethuahinhtron
+    {
+        static void Main(string[] args)
+        {
+            HinhTru x = new HinhTru(10, 5);
+            Console.Write("Dien tich xung quanh va toan phan cua hinh tru la: {0}, {1}", x.dientichxungquanh(), x.dientichtoanphan());
+            Console.ReadLine();
+        }
+    }
+}
